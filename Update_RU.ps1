39e31ca5be75346e6d724ff9c16bc3ad9b48282a
Ddarkover <79401832+Ddarkover@users.noreply.github.com>
2024-05-11 15:50:48 +0500

    Add files via upload

---
+++ Update_RU.ps1
@@ -0,0 +1,24 @@
+# Задаем адреса для загрузки файлов
+$urlGeosite = "https://github.com/Nidelon/ru-block-v2ray-rules/raw/release/geosite.dat"
+$urlGeoip = "https://github.com/Nidelon/ru-block-v2ray-rules/raw/release/geoip.dat"
+
+# Функция для загрузки файла
+function DownloadFile($url, $outputPath) {
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile($url, $outputPath)
+}
+
+# Скачиваем файлы и переименовываем их
+DownloadFile $urlGeosite "geosite_RU.dat"
+DownloadFile $urlGeoip "geoip_RU.dat"
+
+# Выводим сообщение об успешном завершении загрузки и переименования файлов
+Write-Host "The files have been successfully uploaded and renamed."
+
+# Перемещаем файлы в папку со скриптом
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Move-Item "geosite_RU.dat" "$scriptDirectory\geosite_RU.dat"
+Move-Item "geoip_RU.dat" "$scriptDirectory\geoip_RU.dat"
+
+# Выводим сообщение об успешном завершении перемещения файлов
+Write-Host "The files have been successfully moved to the script folder."
