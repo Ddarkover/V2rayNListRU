3be0dc72c0804f92dc59ea9c3f7f6575a33a327c
Ddarkover <79401832+Ddarkover@users.noreply.github.com>
2024-06-08 14:45:37 +0500

    Update Update_RU.ps1

--- Update_RU.ps1
+++ Update_RU.ps1
@@ -4,22 +4,16 @@ $urlGeoip = "https://github.com/Nidelon/ru-block-v2ray-rules/raw/release/geoip.d
 
 # Функция для загрузки файла
 function DownloadFile($url, $outputPath) {
-    $webClient = New-Object System.Net.WebClient
-    $webClient.DownloadFile($url, $outputPath)
+    Invoke-WebRequest -Uri $url -OutFile $outputPath
     Write-Host "Successfully downloaded $url to $outputPath"
 }
 
-# Скачиваем файлы и переименовываем их
-DownloadFile $urlGeosite "geosite_RU.dat"
-DownloadFile $urlGeoip "geoip_RU.dat"
-
-# Выводим сообщение об успешном завершении загрузки и переименования файлов
-Write-Host "The files have been successfully uploaded and renamed."
-
-# Перемещаем файлы в папку со скриптом
+# Определяем путь к папке со скриптом
 $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
-Move-Item "geosite_RU.dat" "$scriptDirectory\geosite_RU.dat"
-Move-Item "geoip_RU.dat" "$scriptDirectory\geoip_RU.dat"
 
-# Выводим сообщение об успешном завершении перемещения файлов
-Write-Host "The files have been successfully moved to the script folder."
+# Скачиваем файлы и перемещаем их в папку со скриптом
+DownloadFile $urlGeosite "$scriptDirectory\geosite_RU.dat"
+DownloadFile $urlGeoip "$scriptDirectory\geoip_RU.dat"
+
+# Выводим сообщение об успешном завершении загрузки и перемещения файлов
+Write-Host "The files have been successfully downloaded and moved to the script folder."
